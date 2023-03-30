bc6c634878acb430373ecf94e3aa3c99888cf891
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-03-30 20:32:28 +0300

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
-import axios from "axios";
+// import axios from "axios";
 
 
-axios.defaults.baseURL = "https://pixabay.com/api/";
+// axios.defaults.baseURL = "https://pixabay.com/api/";
 
 
-export const fetchImagesWithQuery = async query => {
-  const response = axios.get(`/search?query=${searchQuery}`);
-  return response.data.hits;
-};
+// export const fetchImagesWithQuery = async query => {
+//   const response = axios.get(`/search?query=${searchQuery}`);
+//   return response.data.hits;
+// };
 
-export default {
-  fetchArticlesWithQuery,
-};
+// export default {
+//   fetchArticlesWithQuery,
+// };
