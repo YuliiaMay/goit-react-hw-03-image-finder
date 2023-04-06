2be0535c51e2b76093ebe8c343826fefcd5dec0f
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-04-06 14:11:35 +0300

    fix scroll

--- src/helpers/Scroll.js
+++ src/helpers/Scroll.js
@@ -1,9 +1,11 @@
 
-function scrollPageDown() {
+const scrollPageDown = () => {
+    setTimeout(() => {
         window.scrollTo({
-            top: document.documentElement.scrollHeight,
-            behavior: 'smooth',
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth',
         });
+    }, 0);
 }
 
 export default scrollPageDown;
